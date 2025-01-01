395c2f9338dc75607ede53b4dff163742d9a3696
DongA Shin <82761686+DoItDoA@users.noreply.github.com>
2025-01-01 19:52:30 +0900

    백준 2559번 문제

---
+++ BOJ/src/two_pointer/p2559/Main.java
@@ -0,0 +1,33 @@
+package two_pointer.p2559;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+
+public class Main {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        String[] input = br.readLine().split(" ");
+        int n = Integer.parseInt(input[0]);
+        int k = Integer.parseInt(input[1]);
+
+        int[] nums = new int[n];
+        input = br.readLine().split(" ");
+        for (int i = 0; i < n; i++) {
+            nums[i] = Integer.parseInt(input[i]);
+        }
+
+        int sum = 0;
+        for (int i = 0; i < k; i++) {
+            sum += nums[i];
+        }
+
+        int max = sum;
+        for (int i = k; i < n; i++) {
+            sum += nums[i];
+            sum -= nums[i - k];
+            max = Math.max(max, sum);
+        }
+        System.out.println(max);
+    }
+}
