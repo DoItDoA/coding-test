ad6caf94c7906e1bf2b881a1cd0b0125cbb3f80b
DongA Shin <82761686+DoItDoA@users.noreply.github.com>
2025-01-09 01:33:23 +0900

    백준 2839번 문제

---
+++ BOJ/src/greedy/p2839/Main.java
@@ -0,0 +1,31 @@
+package greedy.p2839;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+
+public class Main {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        int n = Integer.parseInt(br.readLine());
+
+        int cnt = 0;
+        int max = n / 5;
+        int sugar = n;
+        for (int k = 0; k <= max; k++) {
+            sugar = n;
+            cnt = 0;
+            for (int i = 0; i < max - k; i++) {
+                sugar -= 5;
+                cnt++;
+            }
+            while (true) {
+                if (sugar < 3 || sugar == 0) break;
+                sugar -= 3;
+                cnt++;
+            }
+            if (sugar == 0) break;
+        }
+        System.out.println(sugar == 0 ? cnt : -1);
+    }
+}
