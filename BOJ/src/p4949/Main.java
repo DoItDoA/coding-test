3c4fbef050c294cd53b8cadfbe1176c57194196d
DongA Shin <82761686+DoItDoA@users.noreply.github.com>
2025-01-10 03:41:51 +0900

    백준 문제

--- BOJ/src/p4949/Main.java
+++ BOJ/src/p4949/Main.java
@@ -12,42 +12,27 @@ public class Main {
         while (true) {
             String input = br.readLine();
             if (input.equals(".")) break;
-
             String result = "yes";
-            int[] balance = new int[4];
-            Stack<Character> stack = new Stack<>();
-            for (int i = 0; i < input.length(); i++) {
-                char c = input.charAt(i);
-                if (c == '(') {
-                    balance[0]++;
-                    stack.add(')');
-                } else if (c == ')') {
-                    balance[1]++;
+            if (!isValid(input)) result = "no";
 
-                } else if (c == '[') {
-                    balance[2]++;
-                    stack.add(']');
-                } else if (c == ']') {
-                    balance[3]++;
-
-                }
-                if (balance[1] > balance[0] || balance[3] > balance[2]) {
-                    result = "no";
-                    break;
-                }
-
-                if (c == ')' && stack.pop() != ')') {
-                    result = "no";
-                    break;
-                } else if (c == ']' && stack.pop() != ']') {
-                    result = "no";
-                    break;
-                }
-            }
-            if (!(balance[1] == balance[0] && balance[3] == balance[2])) {
-                result = "no";
-            }
             System.out.println(result);
         }
     }
+
+
+    private static boolean isValid(String input) {
+        Stack<Character> s = new Stack<>();
+        for (int i = 0; i < input.length(); i++) {
+            char c = input.charAt(i);
+            if (c == '(' || c == '[') {
+                s.add(c);
+            } else if (c == ')') {
+                if (s.isEmpty() || s.pop() != '(') return false;
+            } else if (c == ']') {
+                if (s.isEmpty() || s.pop() != '[') return false;
+            }
+        }
+        return s.isEmpty();
+    }
 }
+
