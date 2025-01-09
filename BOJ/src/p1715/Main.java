035c65e57b52eed8917d832d3d4f2dda4f5940fc
DongA Shin <82761686+DoItDoA@users.noreply.github.com>
2025-01-09 21:42:06 +0900

    백준 1715번 문제

--- BOJ/src/p1715/Main.java
+++ BOJ/src/p1715/Main.java
@@ -1,16 +1,28 @@
 package p1715;
-// 어려움
+
 import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
+import java.util.PriorityQueue;
 
 public class Main {
     public static void main(String[] args) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-
         int n = Integer.parseInt(br.readLine());
+        PriorityQueue<Integer> q = new PriorityQueue<>();
+        for (int i = 0; i < n; i++) {
+            q.add(Integer.parseInt(br.readLine()));
+        }
 
+        int total = 0;
+        while (q.size() > 1) {
+            Integer first = q.poll();
+            Integer second = q.poll();
+            int sum = first + second;
+            total += sum;
+            q.add(sum);
+        }
 
-
+        System.out.println(total);
     }
 }
