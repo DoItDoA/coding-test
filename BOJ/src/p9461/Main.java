2cce9a5c91fdb5524232de5309a1f83ac3bc2880
DongA Shin <82761686+DoItDoA@users.noreply.github.com>
2025-01-14 16:26:31 +0900

    백준 문제

---
+++ BOJ/src/p9461/Main.java
@@ -0,0 +1,25 @@
+package p9461;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+
+public class Main {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        int t = Integer.parseInt(br.readLine());
+        for (int test = 0; test < t; test++) {
+            int n = Integer.parseInt(br.readLine());
+            long[] dp = new long[n + 2];
+            dp[0] = 1;
+            dp[1] = 1;
+            dp[2] = 1;
+
+            for (int i = 3; i < n; i++) {
+                dp[i] = dp[i - 2] + dp[i - 3];
+            }
+
+            System.out.println(dp[n - 1]);
+        }
+    }
+}
