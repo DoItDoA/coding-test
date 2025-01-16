329e61dcbc75888a970f40ce831504593a048845
DongA Shin <82761686+DoItDoA@users.noreply.github.com>
2025-01-16 18:17:16 +0900

    백준 문제

--- BOJ/src/p11660/Main.java
+++ BOJ/src/p11660/Main.java
@@ -12,21 +12,28 @@ public class Main {
         int m = Integer.parseInt(input[1]);
 
         int[][] map = new int[n][n];
-        int[][] xy = new int[m][4];
+        int[][] prefixSum = new int[n + 1][n + 1];
         for (int i = 0; i < n; i++) {
             input = br.readLine().split(" ");
             for (int j = 0; j < n; j++) {
                 map[i][j] = Integer.parseInt(input[j]);
+                prefixSum[i + 1][j + 1] = prefixSum[i + 1][j] + map[i][j];
             }
         }
 
+        StringBuilder sb = new StringBuilder();
         for (int i = 0; i < m; i++) {
             input = br.readLine().split(" ");
-            for (int j = 0; j < 4; j++) {
-                xy[i][j] = Integer.parseInt(input[j]);
+            int x1 = Integer.parseInt(input[0]);
+            int y1 = Integer.parseInt(input[1]);
+            int x2 = Integer.parseInt(input[2]);
+            int y2 = Integer.parseInt(input[3]);
+            int sum = 0;
+            for (int j = x1; j <= x2; j++) {
+                sum += prefixSum[j][y2] - prefixSum[j][y1 - 1];
             }
+            sb.append(sum).append("\n");
         }
-
-
+        System.out.println(sb);
     }
 }
