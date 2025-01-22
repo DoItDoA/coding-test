44dbf04fac4cefde556d0ce4aec7faafd045dea8
DongA Shin <82761686+DoItDoA@users.noreply.github.com>
2025-01-22 12:46:33 +0900

    백준 문제

--- BOJ/src/p13460/Main.java
+++ BOJ/src/p13460/Main.java
@@ -1,5 +1,6 @@
 package p13460;
-// 보류
+// 존나 어려움
+
 import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
@@ -7,11 +8,11 @@ import java.util.LinkedList;
 import java.util.Queue;
 
 public class Main {
-    static int n, m;
-    static char[][] map;
-    static int[] dx = {0, 1, 0, -1};
-    static int[] dy = {1, 0, -1, 0};
-
+    private static int n, m;
+    private static char[][] map;
+    private static int[] dy = {0, 1, 0, -1};
+    private static int[] dx = {1, 0, -1, 0};
+    private static boolean[][][][] chk;
 
     public static void main(String[] args) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
@@ -20,6 +21,86 @@ public class Main {
         n = Integer.parseInt(s[0]);
         m = Integer.parseInt(s[1]);
 
+        map = new char[n][m];
+        chk = new boolean[n][m][n][m];
+        int ry = 0, rx = 0, by = 0, bx = 0;
 
+        for (int i = 0; i < n; i++) {
+            char[] chars = br.readLine().toCharArray();
+            for (int j = 0; j < m; j++) {
+                map[i][j] = chars[j];
+                if (map[i][j] == 'R') {
+                    ry = i;
+                    rx = j;
+                    map[i][j] = '.';
+                } else if (map[i][j] == 'B') {
+                    by = i;
+                    bx = j;
+                    map[i][j] = '.';
+                }
+            }
+        }
+
+        System.out.println(bfs(ry, rx, by, bx));
+    }
+
+    private static int bfs(int ry, int rx, int by, int bx) {
+        Queue<int[]> q = new LinkedList<>();
+        q.add(new int[]{ry, rx, by, bx, 0});
+        while (!q.isEmpty()) {
+            int[] poll = q.poll();
+            int cry = poll[0];
+            int crx = poll[1];
+            int cby = poll[2];
+            int cbx = poll[3];
+            int cmove = poll[4];
+            chk[cry][crx][cby][cbx] = true;
+
+            if (cmove >= 10) return -1;
+
+            for (int k = 0; k < 4; k++) {
+                int[] rMove = move(cry, crx, k);
+                int nry = rMove[0];
+                int nrx = rMove[1];
+                int rcnt = rMove[2];
+                int[] bMove = move(cby, cbx, k);
+                int nby = bMove[0];
+                int nbx = bMove[1];
+                int bcnt = bMove[2];
+
+                if (nby == -1 && nbx == -1) {
+                    continue;
+                }
+                if (nry == -1 && nrx == -1) {
+                    return cmove + 1;
+                }
+
+                if (nry == nby && nrx == nbx) {
+                    if (rcnt > bcnt) {
+                        nry -= dy[k];
+                        nrx -= dx[k];
+                    } else {
+                        nby -= dy[k];
+                        nbx -= dx[k];
+                    }
+                }
+                if (!chk[nry][nrx][nby][nbx]) {
+                    chk[nry][nrx][nby][nbx] = true;
+                    q.add(new int[]{nry, nrx, nby, nbx, cmove + 1});
+                }
+            }
+        }
+        return -1;
+    }
+
+    private static int[] move(int y, int x, int k) {
+        int cnt = 0;
+        while (map[y][x] != '#') {
+            y = y + dy[k];
+            x = x + dx[k];
+            if (map[y][x] == 'O') return new int[]{-1, -1, 0};
+            cnt++;
+        }
+        return new int[]{y - dy[k], x - dx[k], cnt};
     }
 }
--- BOJ/src/p3190/Main.java
+++ BOJ/src/p3190/Main.java
@@ -1,11 +1,11 @@
 package p3190;
-// 보류
+
 import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
-
+import java.util.Deque;
 import java.util.HashMap;
-
+import java.util.LinkedList;
 import java.util.Map;
 
 public class Main {
@@ -22,8 +22,8 @@ public class Main {
         int k = Integer.parseInt(br.readLine());
         for (int i = 0; i < k; i++) {
             String[] s = br.readLine().split(" ");
-            int y = Integer.parseInt(s[0]);
-            int x = Integer.parseInt(s[1]);
+            int y = Integer.parseInt(s[0]) - 1;
+            int x = Integer.parseInt(s[1]) - 1;
             map[y][x] = 1;
         }
 
@@ -34,5 +34,43 @@ public class Main {
             command.put(Integer.parseInt(s[0]), s[1]);
         }
 
+        Deque<int[]> snake = new LinkedList<>();
+        snake.add(new int[]{0, 0, 0});
+        map[0][0] = 2;
+        int sec = 0;
+        while (!snake.isEmpty()) {
+            int[] head = snake.peekLast();
+            int hy = head[0];
+            int hx = head[1];
+            int hd = head[2];
+
+            if (command.containsKey(sec)) {
+                String c = command.get(sec);
+                if (c.equals("D")) {
+                    hd = (hd + 1) % 4;
+                } else {
+                    hd = (hd + 3) % 4;
+                }
+            }
+
+            int ny = hy + dy[hd];
+            int nx = hx + dx[hd];
+            sec++;
+
+            if (ny >= n || ny < 0 || nx >= n || nx < 0 || map[ny][nx] == 2) break;
+
+            snake.addLast(new int[]{ny, nx, hd});
+
+            if (map[ny][nx] == 1) {
+                map[ny][nx] = 2;
+            } else {
+                map[ny][nx] = 2;
+                int[] tail = snake.removeFirst();
+                map[tail[0]][tail[1]] = 0;
+            }
+
+
+        }
+        System.out.println(sec);
     }
 }
