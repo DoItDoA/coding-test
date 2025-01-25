004a397a904f3e1b37bf893c576b2ea16171581a
DongA Shin <82761686+DoItDoA@users.noreply.github.com>
2025-01-26 08:51:47 +0900

    백준 문제

---
+++ BOJ/src/p11403/Main.java
@@ -0,0 +1,39 @@
+package p11403;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.StringTokenizer;
+
+public class Main {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        int n = Integer.parseInt(br.readLine());
+        int[][] dp = new int[n][n];
+
+        for (int i = 0; i < n; i++) {
+            StringTokenizer st = new StringTokenizer(br.readLine());
+            for (int j = 0; j < n; j++) {
+                dp[i][j] = Integer.parseInt(st.nextToken());
+            }
+        }
+
+        for (int k = 0; k < n; k++) {
+            for (int i = 0; i < n; i++) {
+                for (int j = 0; j < n; j++) {
+                    if (dp[i][k] != 0 && dp[k][j] != 0) {
+                        dp[i][j] = 1;
+                    }
+                }
+            }
+        }
+        StringBuilder sb = new StringBuilder();
+        for (int i = 0; i < n; i++) {
+            for (int j = 0; j < n; j++) {
+                sb.append(dp[i][j] + " ");
+            }
+            sb.append("\n");
+        }
+        System.out.println(sb);
+    }
+}
